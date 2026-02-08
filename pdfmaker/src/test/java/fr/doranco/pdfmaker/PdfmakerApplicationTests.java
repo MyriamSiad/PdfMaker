774cb7a9faf1cefd4d27bf5424c36bc7d8c26cd5
Myriam <siadmyriam@gmail.com>
2026-02-08 11:48:36 +0100

    First Commit du projet de soutenance

---
+++ pdfmaker/src/test/java/fr/doranco/pdfmaker/PdfmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.doranco.pdfmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
