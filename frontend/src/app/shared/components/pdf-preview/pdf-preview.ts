80eacb9a2a5fa9a4e3492e827c5eb5890f17b77c
Myriam <siadmyriam@gmail.com>
2026-03-18 14:35:52 +0100

    Ajout de angular

---
+++ frontend/src/app/shared/components/pdf-preview/pdf-preview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-preview',
+  imports: [],
+  templateUrl: './pdf-preview.html',
+  styleUrl: './pdf-preview.css',
+})
+export class PdfPreview {
+
+}
